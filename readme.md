b2aa8a4c07abf8d56452e44a8af992628f21b1d2
Nino Ding <ninoding9@gmail.com>
2021-10-19 18:00:00 -0400

    Life

---
+++ readme.md
@@ -0,0 +1,22 @@
+Mengxuan(Nino) Ding
+IntelliJ IDEA Community Edition 2021.2.1
+kotlinc-jvm 1.8
+Windows 10 Home
+
+In my version of Conway’s Game of Life:
+(1) A pattern could only be drawn on the grid only if there is enough sapce for the pattern.
+For example, if you select Block as you pattern and you want to draw the block on the most top right 
+cell, the Block will not appear on th grid since there are only 2 cells left but a Block needs 4 cells.
+(2) A pattern could be drawn on the top of others. (Overlapping is allowed)
+
+
+Hotkey Settings:
+
+Pause: Pause the animation and swtich to manual mode.
+Pause	<- P Button
+
+Next: Advance one frame at a time only when manual mode is on.
+Next 	<- N Button 
+
+Resume: Resume the animation when manual mode is on.
+Resume	<- R Button
---
+++ src/main/kotlin/StatusView.kt
@@ -0,0 +1,45 @@
+import javafx.geometry.Pos
+import javafx.scene.control.Label
+import javafx.scene.layout.HBox
+import javafx.scene.layout.Priority
+import javafx.scene.text.Text
+import javafx.scene.text.TextAlignment
+import javafx.scene.control.TextArea
+import java.util.Collections.addAll
+
+class StatusView(private val model: Model) : IView, HBox() {
+    var actionInfo = Label("")
+    var frameInfo = Label("")
+
+
+    init {
+        var frameNum = model.getFrameCounter()
+
+        frameInfo.text = "Frame $frameNum"
+
+        actionInfo.setMinSize(750.0, 20.0)
+        frameInfo.setMinSize(100.0, 20.0)
+
+        this.children.addAll(actionInfo, frameInfo)
+    }
+
+    override fun update() {
+        // react to update from model
+        // how do we get data from the model? do we need it?
+        // println("StatusView is updated")
+        var shapeInfo = model.getSelectedShape()
+        var xPos = model.getClick().first
+        var yPos = model.getClick().second
+        var frameNum = model.getFrameCounter()
+        // init gets called after constructors
+        // you can set up here
+        if (shapeInfo == "Clear" || shapeInfo == "") {
+            actionInfo.text = "Clear"
+        }
+        if (model.getStatusUpdate()) {
+            actionInfo.text = "Created $shapeInfo at $xPos,$yPos"
+        }
+        frameInfo.text = "Frame $frameNum"
+        model.setStatusUpdate(false)
+    }
+}
